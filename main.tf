f4cc224479607e3b035ced9c98c924b828ccbdb0
Torgayev Tamirlan <torgayev_tamirlan_xb@cyberagent.co.jp>
2019-07-04 21:00:33 +0900

    Initialize repository

---
+++ main.tf
@@ -0,0 +1,100 @@
+locals {
+  package_filename = "package.zip"
+}
+
+data "external" "package" {
+  program = ["curl", "-O", "${local.package_filename}", "${var.lambda_package_url}"]
+}
+
+data "aws_s3_bucket" "logs" {
+  bucket = "${var.log_bucket}"
+}
+
+resource "aws_lambda_function" "function" {
+  function_name = "${var.name}"
+  handler       = "${var.handler}"
+  role          = "${module.iam.arn}"
+  runtime       = "${var.runtime}"
+  memory_size   = "${var.memory}"
+  timeout       = "${var.timeout}"
+
+  filename = "${local.package_filename}"
+
+  tracing_config {
+    mode = "${var.tracing_mode}"
+  }
+
+  environment {
+    variables {
+      LOG_ID_FIELD        = "${var.log_id_field}"
+      LOG_TYPE_FIELD      = "${var.log_type_field}"
+      LOG_TIMESTAMP_FIELD = "${var.log_timestamp_field}"
+
+      LOG_S3_BUCKET = "${var.log_bucket}"
+      LOG_S3_PREFIX = "${var.log_path_prefix}"
+    }
+  }
+
+  tags = "${var.tags}"
+}
+
+resource "aws_lambda_event_source_mapping" "kinesis_mapping" {
+  batch_size        = "${var.max_batch_size}"
+  event_source_arn  = "${var.kinesis_stream_arn}"
+  enabled           = true
+  function_name     = "${aws_lambda_function.function.arn}"
+  starting_position = "${var.starting_position}"
+}
+
+resource "aws_iam_role_policy_attachment" "xray_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"
+  role       = "${module.iam.name}"
+}
+
+module "iam" {
+  source = "baikonur-oss/aws-iam_nofile?ref=v1.0.0"
+  type   = "lambda"
+  name   = "${var.name}"
+
+  policy_json = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kinesis:DescribeStream",
+                "kinesis:GetShardIterator",
+                "kinesis:GetRecords",
+                "kinesis:ListStreams"
+            ],
+            "Resource": [
+                "${var.kinesis_stream_arn}"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:*:*:*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "${data.aws_s3_bucket.logs.arn}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
---
+++ variables.tf
@@ -0,0 +1,74 @@
+variable "memory" {
+  description = "Lambda Function memory in megabytes"
+  default     = 256
+}
+
+variable "timeout" {
+  description = "Lambda Function timeout in seconds"
+  default     = 60
+}
+
+variable "max_batch_size" {
+  description = "Maximum number of records passed for a single Lambda invocation"
+}
+
+variable "kinesis_stream_arn" {
+  description = "Source Kinesis Data Streams stream name"
+}
+
+variable "log_bucket" {
+  description = "Target S3 bucket to save data to"
+}
+
+variable "log_path_prefix" {
+  description = "Log file path prefix"
+}
+
+variable "lambda_package_url" {
+  description = "Lambda package URL (see Usage in README)"
+}
+
+variable "handler" {
+  description = "Lambda Function handler (entrypoint)"
+  default     = "main.handler"
+}
+
+variable "runtime" {
+  description = "Lambda Function runtime"
+  default     = "python3.6"
+}
+
+variable "name" {
+  description = "Resource name"
+}
+
+variable "starting_position" {
+  description = "see https://docs.aws.amazon.com/kinesis/latest/APIReference/API_GetShardIterator.html"
+  default     = "TRIM_HORIZON"
+}
+
+variable "log_id_field" {
+  description = "Key name for unique log ID"
+  default     = "log_id"
+}
+
+variable "log_type_field" {
+  description = "Key name for log type"
+  default     = "log_type"
+}
+
+variable "log_timestamp_field" {
+  description = "Key name for log timestamp"
+  default     = "time"
+}
+
+variable "tracing_mode" {
+  description = "X-Ray tracing mode (see: https://docs.aws.amazon.com/lambda/latest/dg/API_TracingConfig.html )"
+  default     = "PassThrough"
+}
+
+variable "tags" {
+  description = "Resource tags"
+  type        = "map"
+  default     = {}
+}
